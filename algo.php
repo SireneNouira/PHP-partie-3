50f51a80f866ce4436e1632230fd4c02dcb74eaf
SireneNouira <sirene.nouira16@gmail.com>
2024-12-03 16:24:57 +0100

    finis

---
+++ algo.php
@@ -0,0 +1,73 @@
+<?php
+
+//Exercice 1
+
+// $variable = 0;
+
+
+// while ($variable < 10) {
+//     echo $variable . "\n";
+//     $variable++;
+// }
+
+//Exercice 2
+
+// $var = 0;
+// $var2 = 89;
+
+// while($var <= 20){
+//   $result =  $var * $var2;
+//     echo $result . "\n";
+//     $var++;
+// }
+
+//Exercice 3
+
+// $var = 100;
+// $var2 = 89;
+// while($var >= 10){
+//   $result =  $var * $var2;
+//     echo $result . "\n";
+//     $var--;
+// }
+
+//Exercice 4
+
+// $var = 1;
+// while($var <= 10){
+//     echo $var . "\n";
+//     $var += $var / 2;
+// }
+
+//Exercice 5
+
+
+// $var = 1;
+// while($var <= 15){
+//     echo $var . "\n" . "on y arrivera";
+//     $var ++;
+// }
+
+//Exercice 6
+
+// $var = 20;
+// while($var >= 0){
+//     echo  $var . "\n" . "on y presque bon ";
+//     $var --;
+// }
+
+//Exercice 7
+
+// $var = 1;
+// while($var <= 100){
+//     echo  $var . "\n" . "On tient le bon bout. ";
+//     $var+=15;
+// }
+
+//Exercice 8
+
+// $var = 200;
+// while($var >= 0){
+//     echo  $var . "\n" . "Enfin !!!!.";
+//     $var-=12;
+// }
